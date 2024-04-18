ec9d397a013e1670521c1510a02c1fa1b30ed1d0
zhangtianyi03 <zhangtianyi03@inspur.com>
2024-04-18 17:28:08 +0800

    Create Unit Test for Home Controller

--- src/test/java/basic/mvc/demo/home/HomeControllerTest.java
+++ src/test/java/basic/mvc/demo/home/HomeControllerTest.java
@@ -1,4 +1,47 @@
 package basic.mvc.demo.home;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = "classpath:spring-mvc.xml")
 public class HomeControllerTest {
+
+    MockMvc mvc;
+
+    @Autowired
+    WebApplicationContext context;
+
+    @Before
+    public void before() {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void index() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isFound())
+                .andDo(print());
+    }
+
+    @Test
+    public void home() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/home"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
 }
