a44cb75765f3d048e3f6fc2199d173ab4c77205d
zhangtianyi03 <zhangtianyi03@inspur.com>
2024-04-19 09:56:51 +0800

    Import Jdbc Dependency - Wait for Oracle Driver Downloading

--- src/main/java/basic/mvc/utility/BaseDao.java
+++ src/main/java/basic/mvc/utility/BaseDao.java
@@ -1,4 +1,12 @@
 package basic.mvc.utility;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.annotation.Resource;
+
 public abstract class BaseDao<T> {
+
+    @Resource
+    protected JdbcTemplate jdbcTemplate;
+
 }
