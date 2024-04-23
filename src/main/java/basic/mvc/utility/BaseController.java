c8d8eadc21863beb089f6dbb94b58dd640b1a80c
zhangtianyi03 <zhangtianyi03@inspur.com>
2024-04-23 10:55:54 +0800

    Init Base Controller Test Based On Mockito

--- src/main/java/basic/mvc/utility/BaseController.java
+++ src/main/java/basic/mvc/utility/BaseController.java
@@ -1,25 +1,57 @@
 package basic.mvc.utility;
 
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.util.HtmlUtils;
+import org.springframework.web.util.JavaScriptUtils;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class BaseController {
+public abstract class BaseController {
 
     @Resource
-    protected HttpServletRequest request;
+    public HttpServletRequest request;
 
-    protected SqlSessionFactoryBean sqlSessionFactoryBean;
+    public SqlSessionFactoryBean sqlSessionFactoryBean;
 
-    protected final Logger logger = Logger.getLogger(this.getClass());
+    public final Logger logger = Logger.getLogger(this.getClass());
 
-    protected void renderJson(HttpServletResponse response, String text) {
-        response.setContentType("application/json;charset=UTF-8");
+    public String getPara(String name) {
+        return this.getPara(name, "");
+    }
+
+    public String getPara(String name, String defaultValue) {
+        String str = this.request.getParameter(name);
+        str = str.trim();
+        str = StringEscapeUtils.escapeSql(str);
+        str = HtmlUtils.htmlEscape(str);
+        str = JavaScriptUtils.javaScriptEscape(str);
+        return StringUtils.isNotBlank(str) ? str : defaultValue;
+    }
+
+    public void renderText(HttpServletResponse response, String text) {
+        this.render(response, "text/plain;charset=UTF-8", text);
+    }
+
+    public void renderJson(HttpServletResponse response, String text) {
+        this.render(response, "application/json;charset=UTF-8", text);
+    }
+
+    public void renderXml(HttpServletResponse response, String text) {
+        this.render(response, "text/xml;charset=UTF-8", text);
+    }
+
+    public void renderHtml(HttpServletResponse response, String text) {
+        this.render(response, "text/html;charset=UTF-8", text);
+    }
+
+    public void render(HttpServletResponse response, String contentType, String text) {
+        response.setContentType(contentType);
         response.setHeader("Pragma", "No-cache");
         response.setHeader("Cache-Control", "no-cache");
         response.setDateHeader("Expires", 0L);
--- src/test/java/basic/mvc/utility/BaseControllerTest.java
+++ src/test/java/basic/mvc/utility/BaseControllerTest.java
@@ -1,4 +1,136 @@
 package basic.mvc.utility;
 
-public class BaseControllerTest {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BaseControllerTest extends BaseController {
+
+    MockHttpServletResponse response;
+
+    @Before
+    public void before() {
+        this.request = mock(HttpServletRequest.class);
+        this.response = new MockHttpServletResponse(); // No need to .reset() after committing the response in content acquiring
+        when(this.request.getParameter("Key0")).thenReturn("Value");
+        when(this.request.getParameter("Key1")).thenReturn("");
+        when(this.request.getParameter("Key2")).thenReturn("     ");
+    }
+
+    @Test
+    public void getPara0() {
+        String res = this.getPara("Key0");
+        assertEquals("Value", res);
+    }
+
+    @Test
+    public void getPara1() {
+        String res = this.getPara("Key1");
+        assertEquals("", res);
+    }
+
+    @Test
+    public void getPara2() {
+        String res = this.getPara("Key2");
+        assertEquals("", res);
+    }
+
+    @Test
+    public void getPara3() {
+        String res = this.getPara("Key0", "DefaultValue");
+        assertEquals("Value", res);
+    }
+
+    @Test
+    public void getPara4() {
+        String res = this.getPara("Key1", "DefaultValue");
+        assertEquals("DefaultValue", res);
+    }
+
+    @Test
+    public void getPara5() {
+        String res = this.getPara("Key2", "DefaultValue");
+        assertEquals("DefaultValue", res);
+    }
+
+    @Test
+    public void renderText() throws Exception {
+        this.renderText(response, "Text");
+        assertEquals("text/plain;charset=UTF-8", response.getContentType());
+        assertTrue(response.containsHeader("Pragma"));
+        assertTrue(response.containsHeader("Cache-Control"));
+        assertTrue(response.containsHeader("Expires"));
+        assertEquals("No-cache", response.getHeader("Pragma"));
+        assertEquals("no-cache", response.getHeader("Cache-Control"));
+        assertEquals(0L, response.getDateHeader("Expires"));
+        assertEquals("Text", response.getContentAsString());
+    }
+
+    @Test
+    public void renderJson() throws Exception {
+        this.renderJson(response, "Json");
+        assertEquals("application/json;charset=UTF-8", response.getContentType());
+        assertTrue(response.containsHeader("Pragma"));
+        assertTrue(response.containsHeader("Cache-Control"));
+        assertTrue(response.containsHeader("Expires"));
+        assertEquals("No-cache", response.getHeader("Pragma"));
+        assertEquals("no-cache", response.getHeader("Cache-Control"));
+        assertEquals(0L, response.getDateHeader("Expires"));
+        assertEquals("Json", response.getContentAsString());
+    }
+
+    @Test
+    public void renderXml() throws Exception {
+        this.renderXml(response, "Xml");
+        assertEquals("text/xml;charset=UTF-8", response.getContentType());
+        assertTrue(response.containsHeader("Pragma"));
+        assertTrue(response.containsHeader("Cache-Control"));
+        assertTrue(response.containsHeader("Expires"));
+        assertEquals("No-cache", response.getHeader("Pragma"));
+        assertEquals("no-cache", response.getHeader("Cache-Control"));
+        assertEquals(0L, response.getDateHeader("Expires"));
+        assertEquals("Xml", response.getContentAsString());
+    }
+
+    @Test
+    public void renderHtml() throws Exception {
+        this.renderHtml(response, "Html");
+        assertEquals("text/html;charset=UTF-8", response.getContentType());
+        assertTrue(response.containsHeader("Pragma"));
+        assertTrue(response.containsHeader("Cache-Control"));
+        assertTrue(response.containsHeader("Expires"));
+        assertEquals("No-cache", response.getHeader("Pragma"));
+        assertEquals("no-cache", response.getHeader("Cache-Control"));
+        assertEquals(0L, response.getDateHeader("Expires"));
+        assertEquals("Html", response.getContentAsString());
+    }
+
+    @Test
+    public void render() throws Exception {
+        this.render(response, "content/render;charset=UTF-8", "Render");
+        assertEquals("content/render;charset=UTF-8", response.getContentType());
+        assertTrue(response.containsHeader("Pragma"));
+        assertTrue(response.containsHeader("Cache-Control"));
+        assertTrue(response.containsHeader("Expires"));
+        assertEquals("No-cache", response.getHeader("Pragma"));
+        assertEquals("no-cache", response.getHeader("Cache-Control"));
+        assertEquals(0L, response.getDateHeader("Expires"));
+        assertEquals("Render", response.getContentAsString());
+    }
+
+    @After
+    public void after() {
+        reset(request);
+    }
+
 }
