53b4bae2da1f72201fa46aef69b82b4c4b4b634f
zhangtianyi03 <zhangtianyi03@inspur.com>
2024-04-24 11:11:13 +0800

    Add New Exception

---
+++ src/main/java/basic/mvc/utility/exception/MapPairUnbalancedException.java
@@ -0,0 +1,11 @@
+package basic.mvc.utility.exception;
+
+public class MapPairUnbalancedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public MapPairUnbalancedException(String s) {
+        super(s);
+    }
+
+}
