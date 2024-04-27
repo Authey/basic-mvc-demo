c2e3a959b5d593d1364356c9f9a1cd75e8612048
Authey <1613051286@qq.com>
2024-04-27 15:42:08 +0800

    Update User Controller

--- src/main/java/basic/mvc/demo/user/action/UserController.java
+++ src/main/java/basic/mvc/demo/user/action/UserController.java
@@ -2,13 +2,13 @@ package basic.mvc.demo.user.action;
 
 import basic.mvc.demo.user.service.UserService;
 import basic.mvc.utility.BaseController;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
 
@@ -30,6 +30,8 @@ public class UserController extends BaseController {
         try {
             List<Map<String, Object>> userList = userService.find("SELECT ID, USER_NAME, AUTH_LEVEL FROM SYS_USER");
             logger.info("User List: " + userList);
+            JSONArray json = JSONArray.fromObject(userList);
+            this.renderJson(json.toString());
         } catch (Exception e) {
             logger.error("Cannot Acquire All User Information");
         }
