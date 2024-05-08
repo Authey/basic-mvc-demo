77c1097b865b876cea4ce16d5239dc0d772433e8
zhangtianyi03 <zhangtianyi03@inspur.com>
2024-05-08 15:52:42 +0800

    Optimise Annotation And Import

--- src/main/java/basic/mvc/service/action/DataController.java
+++ src/main/java/basic/mvc/service/action/DataController.java
@@ -1,6 +1,8 @@
 package basic.mvc.service.action;
 
 import basic.mvc.utility.BaseController;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class DataController extends BaseController {
 }
