78ca22ce3abb8f2e44addfe81a73cc273537abb0
zhangtianyi03 <zhangtianyi03@inspur.com>
2024-05-10 10:47:48 +0800

    Update General Service Controller

--- src/main/java/basic/mvc/service/action/GeneralServiceController.java
+++ src/main/java/basic/mvc/service/action/GeneralServiceController.java
@@ -7,7 +7,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
