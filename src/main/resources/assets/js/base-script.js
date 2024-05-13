2d2d3e0d169e55049d235c2ce68945ecca7cfeb2
zhangtianyi03 <zhangtianyi03@inspur.com>
2024-05-13 14:00:10 +0800

    Update Frontend Js Method Invocation Logic

--- src/main/resources/assets/js/base-script.js
+++ src/main/resources/assets/js/base-script.js
@@ -1,3 +1,7 @@
+function alertMsg(component, status, message) {
+    $(component).html(message !== "" ? '<div class="alert alert-' + status +'" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><b>' + message + '</b></div>' : '')
+}
+
 function postMsg(url, message) {
     let form = $("<form>")
     form.attr("style", "display: none")
