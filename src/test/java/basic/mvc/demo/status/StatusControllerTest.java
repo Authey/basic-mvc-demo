3b22bca687fb6e82853589fc78ada230776d0060
zhangtianyi03 <zhangtianyi03@inspur.com>
2024-05-15 18:40:57 +0800

    Update Status Controller Test

--- src/test/java/basic/mvc/demo/status/StatusControllerTest.java
+++ src/test/java/basic/mvc/demo/status/StatusControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.util.NestedServletException;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
